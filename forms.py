b1f5e7a89116c2515f8ca473468c094756b2caed
jockey10 <shane.boulden@gmail.com>
2018-01-16 22:32:19 +1100

    doc forms and update imports

--- forms.py
+++ forms.py
@@ -1,10 +1,15 @@
-
 from flask_wtf import FlaskForm
-from wtforms.fields import *
+from wtforms.fields import TextField, IntegerField
 from wtforms.validators import Required
 
 
 class TestSuiteForm(FlaskForm):
+    """Represents a new test suite form
+
+    This class represents a new test suite form. It uses flask-wtf form fields
+    and validators to construct the form and validate input.
+
+    """
     description = TextField(u'Description')
     url = TextField(u'Test URL')
     nodes = IntegerField(u'Nodes', validators=[Required()])
