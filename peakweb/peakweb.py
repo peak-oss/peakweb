af6b259b6971e95d7e9b5f9b932aee09ef2a2747
Adam Goossens <adam.goossens@gmail.com>
2018-09-01 14:48:50 +1000

    Add a the /healthz endpoint for pod readiness checks

--- peakweb/peakweb.py
+++ peakweb/peakweb.py
@@ -13,6 +13,14 @@ application.secret_key = 'some_secret'
 peakorc = os.environ['PEAKORC']
 
 
+@application.route('/healthz')
+def healthz():
+    """
+    Check the health of this peakweb instance. OCP will hit this endpoint to verify the readiness
+    of the peakweb pod.
+    """
+    return 'OK'
+
 @application.route('/')
 def index():
     return render_template('index.html')
